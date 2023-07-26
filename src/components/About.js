0cdae17060c91e6c1ec5bc2492e139ff5849d15c
Kelly <84016657+telkelly@users.noreply.github.com>
2023-07-26 20:25:31 +0300

    changed styles

--- src/components/About.js
+++ src/components/About.js
@@ -2,23 +2,24 @@ import photo from '../imgs/girl.png';
 
 const About = () => {
     return (
-        <div class="main-info">
-            <div class="personal-photo">
-                 <img class="photo" src={photo}/> 
-            </div>
-
-            <div class="about-person">
-                <h3 class="person-title">
-                    Front-end Developer
-                </h3>
-                <h1>Kelly Teller</h1>
-                <p class="description">Vestibulum sollicitudin eros eget libero lobortis, ac luctus justo lacinia. Suspendisse blandit est quis nibh convallis, ac convallis enim sagittis. Suspendisse leo massa, pretium non accumsan a, hendrerit sollicitudin lorem. Nulla facilisi. Vivamus ac nulla luctus, auctor ligula nec, interdum justo. Suspendisse orci elit, consequat quis tincidunt et, lacinia at ante. Curabitur ac vestibulum lacus, sed condimentum lectus. Nam eleifend semper nulla dignissim condimentum. Pellentesque feugiat, justo a mattis congue, sem erat pretium ante, et egestas massa purus et lectus.</p>
-                <div class="info-btn">
-                <button>Contact me</button>
-                </div>
-            </div>
+      <div class="main-info">
+        <div class="personal-photo">
+          <img class="photo" src={photo} />
         </div>
-    )
+
+        <div class="about-person">
+          <h1>Hi, my name is Kelly Teller</h1>
+          <p class="description">
+            I am a Full Stack Developer, a master of both front-end and back-end
+            sorcery. With a passion for innovation, I conjure captivating web
+            experiences using HTML, CSS, JavaScript, React, Node.js, and more.
+          </p>
+          <div class="info-btn">
+            <button>See for CV</button>
+          </div>
+        </div>
+      </div>
+    );
 }
 
 export default About
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,21 @@
+import { FaGithub,FaLinkedin } from "react-icons/fa";
+
 const Navbar = () => {
   return (
     <header class="nav-bar">
-      <div class="logo">Kelly Teller</div>
+      <div class="logo">Portfolio</div>
       <div class="nav-info">
-        <a>About me</a>
-        <a>Portfolio</a>
+        <a>Projects</a>
         <a>Resume</a>
         <a>Contact</a>
+      </div>
+      <div className="social-media">
+        <a href="https://github.com/telkelly">
+          <FaGithub />
+        </a>
+        <a href="https://www.linkedin.com/in/keltel">
+          <FaLinkedin />
+        </a>
       </div>
     </header>
   );
